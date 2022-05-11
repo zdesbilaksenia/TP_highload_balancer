77c9cf71be8123e7af394da0b470e35354bc610e
zdesbilaksenia <ksju.nikitina@rambler.ru>
2022-05-11 12:26:46 +0300

    server

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func main() {
+	router := echo.New()
+	router.GET("/",
+		func(ctx echo.Context) error {
+			i := time.Duration(rand.Intn(20))
+			time.Sleep(i * time.Millisecond)
+			return ctx.String(http.StatusOK, "server is working")
+		},
+	)
+	log.Fatal(router.Start(":8080"))
+}
