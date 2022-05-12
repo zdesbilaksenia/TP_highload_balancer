5020baafff6eeb9e86768b25d6c693fa5df9d2fe
zdesbilaksenia <ksju.nikitina@rambler.ru>
2022-05-12 22:58:39 +0300

    change histogram

--- main.go
+++ main.go
@@ -7,17 +7,19 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 func main() {
-	reqDur := prometheus.NewHistogram(
+	reqDur := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Subsystem: "",
 			Name:      "request_duration_seconds",
 			Help:      "request latencies",
 			Buckets:   []float64{.005, .01, .02, 0.04, .06, 0.08, .1, 0.15, .25, 0.4, .6, .8, 1, 1.5, 2, 3, 5},
 		},
+		[]string{"code"},
 	)
 
 	prometheus.MustRegister(reqDur)
@@ -25,6 +27,9 @@ func main() {
 	router := echo.New()
 	router.GET("/",
 		func(ctx echo.Context) error {
+			status := strconv.Itoa(http.StatusOK)
+			elapsed := float64(time.Since(time.Now())) / float64(time.Second)
+			reqDur.WithLabelValues(status).Observe(elapsed)
 			i := time.Duration(rand.Intn(500))
 			time.Sleep(i * time.Millisecond)
 			return ctx.String(http.StatusOK, "server is working")
