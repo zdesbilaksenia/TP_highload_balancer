40cf2b14c9468303fb143d3dc2ec1286bac55165
zdesbilaksenia <ksju.nikitina@rambler.ru>
2022-05-12 22:04:59 +0300

    rps

--- main.go
+++ main.go
@@ -14,6 +14,19 @@ func main() {
 	hitsTotal := prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "hits_total",
 	})
+	reqDur := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Subsystem: "",
+			Name:      "request_duration_seconds",
+			Help:      "request latencies",
+			Buckets:   []float64{.005, .01, .02, 0.04, .06, 0.08, .1, 0.15, .25, 0.4, .6, .8, 1, 1.5, 2, 3, 5},
+		},
+		[]string{"code"},
+	)
+
+	if err := prometheus.Register(reqDur); err != nil {
+		log.Fatal(err)
+	}
 	if err := prometheus.Register(hitsTotal); err != nil {
 		log.Fatal(err)
 	}
